d241d5abb0a0dc37b8b551c33a943497992ee7bd
Nick Muravev <67981036+muravevnv@users.noreply.github.com>
2023-10-30 15:54:21 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,8 +3,20 @@
 $('#fullpage').fullpage({
 
   navigation: true,
+  // onLeave: function(index, nextIndex, direction){
+  //   //leaving 1st section
+  //   if(index == 3){
+  //      $('.header').addClass('header--grey');
+  //   }
+  //   //back to the 1st section
+  //   if(nextIndex == 3){
+  //       $('.header').removeClass('header--grey');
+  //   }
+  // } 
 
 }); 
+
+
 
 // phone mask
 
